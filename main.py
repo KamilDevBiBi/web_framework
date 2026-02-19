aeffb6290a06433d1e978e56837c394e4783968a
Kamil <kamil_rr1@mail.ru>
2026-02-19 19:03:56 +0300

    task 4

--- main.py
+++ main.py
@@ -15,5 +15,20 @@ def url2(prof):
 def url3(list):
     return render_template("prof_list.html", list_type=list)
 
+@app.route("/auto_answer")
+@app.route("/answer")
+def url4():
+    params = {}
+    params["title"] = "Watny"
+    params["surname"] = "Кристанг"
+    params["name"] = "Колумб"
+    params["education"] = "Химико-технологическое"
+    params["profession"] = "Кок"
+    params["sex"] = "Мужчина"
+    params["motivation"] = "Ради бесплатной пиццы"
+    params["ready"] = True
+
+    return render_template("auto_answer.html", **params)
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port="8080")
