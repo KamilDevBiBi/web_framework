e2a9a0a65d3cfd219b21015a5f9efd15c0f91ded
Kamil <kamil_rr1@mail.ru>
2026-02-19 18:14:55 +0300

    task 2

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def main_url(title):
     return render_template("base.html", title=title)
 
+@app.route("/training/<prof>")
+def url2(prof):
+    return render_template("training.html", profession=prof)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port="8080")
