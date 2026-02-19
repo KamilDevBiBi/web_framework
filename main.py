5ffa830467cf15d8cebdd2d01af641c9f9f07b26
Kamil <kamil_rr1@mail.ru>
2026-02-19 18:36:16 +0300

    task 3

--- main.py
+++ main.py
@@ -11,5 +11,9 @@ def main_url(title):
 def url2(prof):
     return render_template("training.html", profession=prof)
 
+@app.route("/list_prof/<list>")
+def url3(list):
+    return render_template("prof_list.html", list_type=list)
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port="8080")
